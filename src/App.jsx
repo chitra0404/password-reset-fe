ba7540c6078d7e3bd0a5fc8dfc7c7f7aec88453b
chitra0404 <chitrasuresh0404@gmail.com>
2024-01-02 20:54:10 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import {BrowserRouter as Router,Route, Routes} from 'react-router-dom'
+import Signup from './Components/Signup';
+import Login from './Components/Login';
+import PasswordUpdate from './Components/PasswordUpdate';
+import PasswordReset from './Components/passwordReset';
+
+function App() {
+
+  const url="https://password-reset-backend3.onrender.com";
+
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login url={url}/>}></Route>     
+        <Route path='/Signup' element={<Signup url={url}/>} ></Route>
+        <Route path='/forgotpassword' element={<PasswordReset url={url}/>}></Route>
+        <Route path='/passwordreset/:id' element={<PasswordUpdate url={url}/>}></Route>
+      </Routes>
+      
+    </Router>
+  );
+}
+
+export default App;
